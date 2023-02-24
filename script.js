e422d0960850d39dec9839643b7092f888ee2dba
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-02-25 00:01:15 +0200

    Update

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector("city").innerText = "Weather in " + name;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "01n@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed:" + speed + "km/h";
   },
 };
